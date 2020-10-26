0441d8644f04fdbc3fc1e93d59ef3e039a377cb4
nagaralu <rnsm25@gmail.com>
2020-10-26 08:46:58 -0500

    Handling any missed events

--- event-bus/index.js
+++ event-bus/index.js
@@ -21,6 +21,10 @@ app.post('/events', (req, res) => {
   res.send({ status: 'OK' });
 });
 
+app.get('/events', (req, res) => {
+  res.send(events);
+});
+
 app.listen(4005, () => {
   console.log('Listening on 4005');
 });
--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -42,4 +43,11 @@ app.post('/events', (req, res) => {
 
 app.listen(4002, async () => {
   console.log('Listening on 4002');
+
+  const res = await axios.get('http://event-bus-srv:4005/events');
+
+  for (let event of res.data) {
+    console.log('Processing event:', event.type);
+    handleEvent(event.type, event.data);
+  }
 });
